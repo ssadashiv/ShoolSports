8e08165c318553d2a8b81653a78aeb1f1d1db0c9
lizhenhui <lizhenhui0808@aliyun.com>
2018-05-31 09:24:44 +0800

    高德地图的清除

--- app/src/main/java/com/example/jclzh/shoolsports/view/fragment/MapPathFragment.java
+++ app/src/main/java/com/example/jclzh/shoolsports/view/fragment/MapPathFragment.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.amap.api.maps.MapView;
 import com.example.jclzh.shoolsports.R;
 
 /**
@@ -19,7 +18,6 @@ public class MapPathFragment extends Fragment {
 
 
     private View view;
-    private MapView mapView;
 
     public MapPathFragment() {
     }
@@ -36,39 +34,11 @@ public class MapPathFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map_path, container, false);
-        mapView = view.findViewById(R.id.mhome_map);
-        //初始化地图
-//        mapView.onCreate(savedInstanceState);
 
         return view;
     }
 
 
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
-        mapView.onDestroy();
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
-        mapView.onResume();
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
-        mapView.onPause();
-    }
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
-        mapView.onSaveInstanceState(outState);
-    }
 
 
 }
